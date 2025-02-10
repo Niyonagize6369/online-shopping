8c622b0ee576e812d858663ad91257682e32df41
niyonagize6369 <niyonagizerachel10@gmail.com>
2025-02-10 11:56:57 +0200

    online-shopping pushed

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -2,20 +2,37 @@ import React from "react";
 import LightButton from "../assets/website/Light-mode-button.jpg";
 import DarkButton from "../assets/website/Dark-mode-button.jpg";
 
-const darkMode = () => {
-    
+const DarkMode = () => {
+  const [theme, setTheme] = React.useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+  const element = document.documentElement; //html element
+  console.log(theme);
+  React.useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.element("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
   return (
     <div className="relative">
       <img
         src={LightButton}
         alt=""
-        className="w-12 cursor-pointer drop-shadow-sm 
-              [1px-1px-1px rgba(0,0,0,0.1)] transition-all
-              duration-300 absolute right-0 z-10"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        className={`w-12 cursor-pointer drop-shadow-
+          [1px-1px-1px rgba(0,0,0,0.1)] transition-all
+              duration-300 absolute right-0 z-10 ${
+                theme === "dark" ? "opacity-0" : "opacity-100"
+              }`}
       />
       <img
         src={DarkButton}
         alt=""
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         className="w-12 cursor-pointer drop-shadow-sm 
               [1px-1px-1px rgba(0,0,0,0.1)] transition-all
               duration-300"
@@ -24,4 +41,4 @@ const darkMode = () => {
   );
 };
 
-export default darkMode;
+export default DarkMode;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,56 @@ import React from "react";
 import Logo1 from "../assets/logo1.jpg";
 import { IoMdSearch } from "react-icons/io";
 import { LuSearchCheck } from "react-icons/lu";
-import { FaCartShopping } from "react-icons/fa6";
-import DarkMode from "./DarkMode";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
+import DarkMode from "./Darkmode.jsx";
+const Menu = [
+  {
+    id: 1,
+    name: "home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/Services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Electronics",
+    link: "/#",
+  },
+];
 
+const dropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Product",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
+  },
+];
 function Navbar() {
   return (
-    <div
-      className="relative z-40 duration-200 shadow-md bg-whitedack:bg-gray-900 dark:text-white"
-    >
+    <div className="relative z-40 duration-200 shadow-md bg-whitedack:bg-gray-900 dark:text-green-600">
       {/* upper nav */}
       <div className="py-2 bg-primary/40">
         <div className="container flex items-center justify-between">
@@ -25,9 +67,7 @@ function Navbar() {
 
           {/* search bar */}
 
-          <div
-            className="flex items-center justify-between gap-4"
-          >
+          <div className="flex items-center justify-between gap-4">
             <div className="relative hidden group sm:block">
               <input
                 type="text"
@@ -37,9 +77,7 @@ function Navbar() {
                 border-gray-300 px-2 py-1 focus:outline-none
                 focus border-1 focus:boeder-primary"
               />
-              <IoMdSearch
-                className="absolute text-gray-500 -translate-y-1/2 group-hover:text-primary top-1/2 right-3"
-              />
+              <IoMdSearch className="absolute text-gray-500 -translate-y-1/2 group-hover:text-primary top-1/2 right-3" />
             </div>
 
             {/* order button */}
@@ -47,16 +85,12 @@ function Navbar() {
               onClick={() => alert("Ordering not available yet")}
               className="flex gap-3 px-4 py-1 text-white transition-all duration-200 rounded-full bg-gradient-to-r from-primary to-secondary item center group "
             >
-              <span
-                className="hidden transition-all duration-200 group-hover:block"
-              >
+              <span className="hidden transition-all duration-200 group-hover:block">
                 Order
               </span>
-              <FaCartShopping
-                className="text-xl text-white cursor-pointer drop-shadow-sm"
-              />
+              <FaCartShopping className="text-xl text-white cursor-pointer drop-shadow-sm" />
             </button>
-            
+
             {/* darckmode switch */}
             <div>
               <DarkMode />
@@ -66,7 +100,38 @@ function Navbar() {
       </div>
 
       {/* lower vav */}
-      <div></div>
+      <div className="flex justify-center">
+        <ul className="items-center hidden gap-4 sm:flex">
+          {Menu.map((data) => (
+            <li key={data.id}>
+              <a
+                href={data.link}
+                className="inline-block px-4 duration-200 hover:text-primary "
+              >
+                {data.name}
+              </a>
+            </li>
+          ))}
+          {/* {simple dropdown and links} */}
+          <li className="relative group">
+            <a href="#" className="flex items-center gap-[2px] py-2">
+              Trending
+              <span>
+                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+              </span>
+            </a>
+            <div>
+              <ul>
+                {dropdownLinks.map((data) => (
+                  <li key={data.id}>
+                    <a href={data.link}>{data.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
